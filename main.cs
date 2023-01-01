5005ee1b20873df250dca6f7ec3ef1f141884973
Jitesh Sureka <48786649+jsureka@users.noreply.github.com>
2023-01-01 06:30:33 +0000

    Initial commit

--- main.cs
+++ main.cs
@@ -1,8 +1,23 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    string myvar = Console.ReadLine();
-    Console.WriteLine (myvar);
-  }
+class Program
+{
+    public static void Main(string[] args)  
+      {  
+       int  n,r,sum=0,temp;      
+       Console.Write("Enter the Number= ");      
+       n= int.Parse(Console.ReadLine());     
+       temp=n;      
+       while(n>0)      
+       {      
+        r=n%10;      
+        sum=sum+(r*r*r);      
+        n=n/10;      
+       }      
+       if(temp==sum)      
+        Console.Write("Armstrong Number.");      
+       else      
+        Console.Write("Not Armstrong Number.");      
+      }  
 }
+
